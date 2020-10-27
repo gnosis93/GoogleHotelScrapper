91ff25f6012c359caa0c27f65a93125818059013
aaronscifo <scifoaaron@yahoo.com>
2020-10-27 11:39:48 +0100

    Base Symfony Command ScapeGoogleHotels (Refactoring)

---
+++ src/Console/Commands/ScrapeGoogleHotels.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Appsilon\LeadsEPL\Console\Commands;
+
+use Appsilon\LeadsEPL\Console\DB\DBConnection;
+use Illuminate\Console\Command ;
+use Illuminate\Support\Collection;
+
+class ScapeGoogleHotels extends Command{
+
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'scrape:google-hotel';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Scrape google hotel listings and save them to a csv file';
+
+     
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('starting operation');
+       
+
+        // dd(  $posts);
+    }
+
+}
